3b3c9da6a3f1b0b9256f8a91a5283edf4a1bd78b
github username <github email address>
2019-05-27 22:08:48 -0700

    finish boilerplate for posts_controller, build out seed data and migrate

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,43 @@
+class Api::PostsController < ApplicationController
+
+  def index
+    @posts = Post.all 
+    render json: @posts
+  end
+
+end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: { errors: { message: "your post did not save"}}
+    end
+  end
+
+  def show
+    render json Post.find_by(id: params[:id])
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post && @post.destroy
+      render status: 204
+    else
+      render json: { errors: "unable to delete this post" }
+    end
+  end
+
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content, :author)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Post.create(name: 'Joy', title: "Stop using my driveway!", content: "I'm tired of people always using my driveway. I installed a sign but apparently that's not enough, so I invested in some spike strips. Hope you all enjoy it!")
+  Post.create(name: 'Sally', title: "It's raining again.", content: "Just thought everyone should know.")
+  Post.create(name: 'Gordon', title: "No good place to have a quiet dinner", content: "There's no good place to eat around here without unsupervised, unruly, screaming, out of control children!!! Sitting around having your glass of wine while your children run under my table is not acceptable!! I'm never coming back to this diner again!")
+  Post.create(name: 'Mack', title: "Dog poop in my yard again!!!", content: "I know who are!!  There's a nice package waiting for you on your front porch!") 
+  Post.create(name: 'Fred', title: "Ugly car on my street", content: "Can someone please move that ugly blue car? I pay a lot money for my home, I don't want to have to look at cars with such low Kelly Blue Book Value.")
